ca625ce790c8003ea73b07c456a818b3b1994918
a.efimova <asi4ka.84@gmail.com>
2020-01-23 11:36:58 +0300

    merge homework 1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
-import React, { Component } from 'react'
-import './scss/app.scss'
+import React, { Component } from 'react';
+import './scss/app.scss';
 
-import Router from './components/Router'
+import Router from './components/Router';
 
 class App extends Component {
-    state = { page: 'auth' }
+    state = { page: 'auth' };
 
     GoToPage = (page) => {
-        this.setState({ page })
-    }
+        this.setState({ page });
+    };
 
     render() {
-        const { page } = this.state
-        return <Router route={page} pageSwitcher={this.GoToPage} />
+        const { page } = this.state;
+        return <Router route={page} pageSwitcher={this.GoToPage} />;
     }
 }
 
-export default App
+export default App;
